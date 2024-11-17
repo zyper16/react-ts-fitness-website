1e9bb25b36b4a1c0507b9c0bac8693c35b554b60
zyper16 <zyper16@gmail.com>
2024-11-17 15:25:05 +0200

    Finished ExerciseCard Component Layout

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,13 +1,47 @@
 import { Link } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Stack, Button, Typography } from "@mui/material";
 
 export default function ExerciseCard({ exercise }) {
   return (
-    <Link to={`/exercise/${exercise.id}`}>
-      <Box>
-        <Typography>{exercise.name}</Typography>
-        <img src={exercise.gifUrl} width="100px" height="100px" />
-      </Box>
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#FFA9A9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#FCC757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        ml="21px"
+        color="#000"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "24px", xs: "20px" } }}
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+      >
+        {exercise.name}
+      </Typography>
     </Link>
   );
 }
