a8eef3c61a0d5402b820a1aa034b1825e983311a
zyper16 <zyper16@gmail.com>
2024-11-19 15:20:39 +0200

    Added logic to ExerciseDetail Page

--- src/components/ExerciseInfo.tsx
+++ src/components/ExerciseInfo.tsx
@@ -1,3 +1,14 @@
-export default function ExerciseInfo() {
-  return <p>Exercise Info</p>;
+import { ExerciseCardProps } from "../types/exercisesTypes";
+import { Box } from "@mui/material";
+
+export default function ExerciseInfo({ exercise }: ExerciseCardProps) {
+  return (
+    <Box>
+      <p>{exercise.bodyPart}</p>
+      <p>{exercise.gifUrl}</p>
+      <img src={exercise.gifUrl} />
+      <p>{exercise.name}</p>
+      <p>{exercise.instructions}</p>
+    </Box>
+  );
 }
