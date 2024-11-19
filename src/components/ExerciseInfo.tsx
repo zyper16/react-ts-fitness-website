581bab99dd0d37150e924e0d78016f06c74248ef
zyper16 <zyper16@gmail.com>
2024-11-19 15:50:12 +0200

    Finished ExerciseInfo Component

--- src/components/ExerciseInfo.tsx
+++ src/components/ExerciseInfo.tsx
@@ -1,14 +1,80 @@
 import { ExerciseCardProps } from "../types/exercisesTypes";
-import { Box } from "@mui/material";
+import { Stack, Typography, Button } from "@mui/material";
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
 
 export default function ExerciseInfo({ exercise }: ExerciseCardProps) {
+  const { bodyPart, gifUrl, name, target, equipment } = exercise;
+
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
+
   return (
-    <Box>
-      <p>{exercise.bodyPart}</p>
-      <p>{exercise.gifUrl}</p>
-      <img src={exercise.gifUrl} />
-      <p>{exercise.name}</p>
-      <p>{exercise.instructions}</p>
-    </Box>
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography
+          sx={{ fontSize: { lg: "64px", xs: "30px" } }}
+          fontWeight={700}
+          textTransform="capitalize"
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{ fontSize: { lg: "24px", xs: "18px" } }}
+          color="#4F4C4C"
+        >
+          Exercises keep you strong.{" "}
+          <span style={{ textTransform: "capitalize" }}>{name}</span> is one of
+          the best <br /> exercises to target your {target}. It will help you
+          improve your <br /> mood and gain energy.
+        </Typography>
+        {extraDetail?.map((item, index) => (
+          <Stack
+            key={`${item.name}-${index}`}
+            direction="row"
+            gap="24px"
+            alignItems="center"
+          >
+            <Button
+              sx={{
+                background: "#FFF2DB",
+                borderRadius: "50%",
+                width: "100px",
+                height: "100px",
+              }}
+            >
+              <img
+                src={item.icon}
+                alt={bodyPart}
+                style={{ width: "50px", height: "50px" }}
+              />
+            </Button>
+            <Typography
+              textTransform="capitalize"
+              sx={{ fontSize: { lg: "30px", xs: "20px" } }}
+            >
+              {item.name}
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
   );
 }
--- src/pages/ExerciseDetail.tsx
+++ src/pages/ExerciseDetail.tsx
@@ -1,15 +1,35 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ExerciseInfo from "../components/ExerciseInfo";
-import ExerciseVideos from "../components/ExerciseInfo";
+import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 import { Box } from "@mui/material";
 import { fetchData, fetchOptions } from "../utilities/fetchData";
+import { ExerciseType } from "../types/exercisesTypes";
 
 export default function ExerciseDetail() {
+  const [exerciseData, setExerciseData] = useState<ExerciseType>({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseDetails = async () => {
+      const exerciseDBUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+      const exerciseDetailsData = await fetchData(
+        `${exerciseDBUrl}/exercises/exercise/${id}`,
+        fetchOptions
+      );
+      console.log(exerciseDetailsData);
+
+      setExerciseData(exerciseDetailsData);
+    };
+    fetchExerciseDetails();
+  }, [id]);
+
   return (
     <Box>
-      <ExerciseInfo />
+      <ExerciseInfo exercise={exerciseData} />
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
