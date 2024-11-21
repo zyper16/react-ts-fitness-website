47a5f8c5586d0ce59a11bd2ecaf77cf187fbc823
zyper16 <zyper16@gmail.com>
2024-11-21 20:58:38 +0200

    Created SimilarExercises Logic

--- src/components/SimilarExercises.tsx
+++ src/components/SimilarExercises.tsx
@@ -1,3 +1,48 @@
-export default function SimilarExercises() {
-  return <p>Similar Exercises</p>;
+import { Box, Typography, Stack } from "@mui/material";
+import ExerciseCard from "./ExerciseCard";
+
+export default function SimilarExercises({
+  targetExercises,
+  equipmentExercises,
+}) {
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
+      >
+        Exercises with same target group:
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "107px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {targetExercises.map(exercise => (
+          <ExerciseCard key={exercise?.id} exercise={exercise} />
+        ))}
+      </Stack>
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
+      >
+        Exercises using same equipment:
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "107px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {equipmentExercises.map(exercise => (
+          <ExerciseCard key={exercise?.id} exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
+  );
 }
