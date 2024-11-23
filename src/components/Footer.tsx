75d52c5f6335368b0e9c82b756b804d7ff960ffb
zyper16 <zyper16@gmail.com>
2024-11-23 16:18:25 +0200

    Finished Website Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,28 @@
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/logo-placeholder.png";
+import getCurrentYear from "../types/getCurrentYear";
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack
+        gap="40px"
+        sx={{ alignItems: "center" }}
+        flexWrap="wrap"
+        px="40px"
+        pt="24px"
+      >
+        <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
+      </Stack>
+      <Typography
+        variant="h5"
+        sx={{ fontSize: { lg: "24px", xs: "20px" } }}
+        mt="41px"
+        textAlign="center"
+        pb="40px"
+      >
+        © {getCurrentYear()} Lungu Andrei
+      </Typography>
+    </Box>
+  );
 }
--- src/types/getCurrentYear.ts
+++
@@ -1,3 +0,0 @@
-export default function getCurrentYear() {
-  return new Date().getFullYear();
-}
